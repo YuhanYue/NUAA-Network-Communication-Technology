ed9ca54da1d9817f3d797bd03dd284d591a40ec3
Hope <yuhan0810@outlook.com>
2020-10-28 22:11:29 +0800

    backup

---
+++ Lab1 Ping/ping/CMakeFiles/ping.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ping.dir/main.c.o"
+  "ping"
+  "ping.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
